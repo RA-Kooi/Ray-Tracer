9a3f62119721f7cd61c850899a9916297509c738
Rafaël Kooi <48814281+RA-Kooi@users.noreply.github.com>
2019-04-15 05:55:13 +0200

    Remove self written math library in favor of GLM

--- libRay/Math/MathLibrary.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef d68b2d50_135d_e514_4edc_67b2fc2a5f49
-#define d68b2d50_135d_e514_4edc_67b2fc2a5f49
-
-#include "Vector2.hpp"
-#include "Vector3.hpp"
-#include "Matrix4x4.hpp"
-
-#endif // d68b2d50_135d_e514_4edc_67b2fc2a5f49
