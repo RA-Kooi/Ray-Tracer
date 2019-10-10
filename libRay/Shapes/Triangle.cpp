49ee1bb1662d7cd05a75a4339c335cc90b3fc818
Rafaël Kooi <48814281+RA-Kooi@users.noreply.github.com>
2019-10-11 01:20:53 +0200

    Add a triangle primitive

---
+++ libRay/Shapes/Triangle.cpp
@@ -0,0 +1,88 @@
+#include "Triangle.hpp"
+
+#include <cfloat>
+
+#include "../Containers/BoundingBox.hpp"
+#include "../Math/Matrix.hpp"
+#include "../Math/Ray.hpp"
+#include "../Math/Vector.hpp"
+#include "../Intersection.hpp"
+#include "../Transform.hpp"
+
+using namespace LibRay::Math;
+
+namespace LibRay::Shapes
+{
+std::optional<Intersection> Triangle::Intersects(Math::Ray const &ray) const
+{
+	// o + t * d = v0 + beta * (v1-v0) + gamma * (v2-v0)
+	// -t * d +  beta * (v1-v0) + gamma * (v2-v0) = o - v0
+	// M * (-t, beta, gamma) = o - v0
+	// (-t, beta, gamma) = inverse(M) * (o-v0)
+	// M = (d, v1-v0, v2-v0)
+	// u =  inverse(M) * (o-v0)
+	// t = -u.x, beta = u.y, gamma = u.z
+	// there's an isect if t > 0 && 0 < beta && 0 < gamma && beta + gamma < 1
+
+	Matrix4x4 const worldToModel = transform.InverseMatrix();
+	Ray const modelRay(
+		Transform::TransformTranslation(worldToModel, ray.Origin()),
+		Transform::TransformDirection(worldToModel, ray.Direction()));
+
+	Vector3 const normal = Vector3(0, 0, 1);
+
+	float const denominator = glm::dot(normal, modelRay.Direction());
+
+	if(-denominator > FLT_EPSILON)
+	{
+		Vector3 const v0(0.0f, 0.5f, 0);
+		Vector3 const v1(0.5f, -0.5f, 0);
+		Vector3 const v2(-0.5f, -0.5f, 0);
+
+		Matrix3x3 const M(modelRay.Direction(), v1 - v0, v2 - v0);
+
+		Vector3 const u = glm::inverse(M) * (modelRay.Origin() - v0);
+
+		float const distance = -u.x;
+		float const beta = u.y;
+		float const gamma = u.z;
+
+		if(distance >= 0.f && beta >= 0.f && gamma >= 0.f && beta + gamma < 1.f)
+		{
+			Vector3 const pos = modelRay.Origin()
+				+ modelRay.Direction()
+				* distance;
+
+			return Intersection(
+				*this,
+				Transform::TransformDirection(transform.Matrix(), normal),
+				Transform::TransformTranslation(transform.Matrix(), pos),
+				{pos.x + 0.5f, pos.y + 0.5f});
+		}
+
+		return std::nullopt;
+	}
+
+	return std::nullopt;
+}
+
+Containers::BoundingBox Triangle::CalculateBoundingBox() const
+{
+	Vector3 const v0(0.0f, 0.5f, 0);
+	Vector3 const v1(0.5f, -0.5f, 0);
+	Vector3 const v2(-0.5f, -0.5f, 0);
+
+	Matrix4x4 const &matrix = transform.Matrix();
+
+	Vector3 const worldV0 = matrix * Vector4(v0, 1);
+	Vector3 const worldV1 = matrix * Vector4(v1, 1);
+	Vector3 const worldV2 = matrix * Vector4(v2, 1);
+
+	Vector3 const min = glm::min(worldV0, glm::min(worldV1, worldV2));
+	Vector3 const max = glm::max(worldV0, glm::max(worldV1, worldV2));
+
+	Vector3 const centroid = (min + max) * 0.5f;
+
+	return Containers::BoundingBox((max - min) * 0.5f, centroid);
+}
+} // namespace LibRay::Shapes
---
+++ libRay/Shapes/Triangle.hpp
@@ -0,0 +1,41 @@
+#ifndef bde1d03a_205c_5b41_3d9e_ae6b7e9fc950
+#define bde1d03a_205c_5b41_3d9e_ae6b7e9fc950
+
+#include <optional>
+#include <type_traits>
+
+#include "../Material/MaterialStore.hpp"
+#include "../API.hpp"
+#include "Shape.hpp"
+
+namespace LibRay
+{
+namespace Math
+{
+class Ray;
+} // namespace  Math
+
+class Intersection;
+
+namespace Shapes
+{
+class LIBRAY_API Triangle final: public Shape
+{
+public:
+	using Shape::Shape;
+
+	std::optional<Intersection> Intersects(Math::Ray const &ray) const override;
+
+	Containers::BoundingBox CalculateBoundingBox() const override;
+};
+
+static_assert(std::is_copy_constructible_v<Triangle>);
+static_assert(!std::is_copy_assignable_v<Triangle>);
+static_assert(!std::is_trivially_copyable_v<Triangle>);
+
+static_assert(std::is_move_constructible_v<Triangle>);
+static_assert(!std::is_move_assignable_v<Triangle>);
+} // namespace Shapes
+} // namespace LibRay
+
+#endif // bde1d03a_205c_5b41_3d9e_ae6b7e9fc950
